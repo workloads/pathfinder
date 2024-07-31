b95168d0b89b1c112a2a2f93e48d2f2eade86c8a
Kerim Satirli <k.satirli@gmail.com>
2024-07-31 23:38:36 +0200

    cleans up

--- src/IMU.h
+++ src/IMU.h
@@ -6,25 +6,24 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef struct imu_st_angles_data_tag
-{
+typedef struct imu_st_angles_data_tag {
   float fYaw;
   float fPitch;
   float fRoll;
 }IMU_ST_ANGLES_DATA;
 
-typedef struct imu_st_sensor_data_float
-{
+typedef struct imu_st_sensor_data_float {
   float X;
   float Y;
   float Z;
-}IMU_ST_SENSOR_DATA_FLOAT;
+} IMU_ST_SENSOR_DATA_FLOAT;
 
 void imuInit();
-void imuDataGet(EulerAngles *pstAngles, 
+void imuDataGet(EulerAngles *pstAngles,
                 IMU_ST_SENSOR_DATA_FLOAT *pstGyroRawData,
                 IMU_ST_SENSOR_DATA_FLOAT *pstAccelRawData,
                 IMU_ST_SENSOR_DATA *pstMagnRawData); 
+
 void imuAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
 float invSqrt(float x);
 
