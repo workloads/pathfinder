9eb440c316c01f2b2b11e0dd4ca3bf0491e733b8
Kerim Satirli <k.satirli@gmail.com>
2024-08-10 22:21:15 +0200

    adds Device functions

---
+++ src/Device.cpp
@@ -0,0 +1,143 @@
+#include "Device.h"
+
+#include "Display.h"
+
+// TODO(ksatirli) re-enable when upstream warning is fixed
+// #include "Insights.h"
+#include "Log.h"
+#include "WifiClient.h"
+#include "WireClient.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// CONFIGURATION                                                              //
+////////////////////////////////////////////////////////////////////////////////
+
+char applicationCompilationString[32];
+
+// TODO(ksatirli) move this to a configuration file
+// see https://dashboard.insights.espressif.com/home/manage-auth-keys/
+const char applicationInsightsKey[] = "<ENTER YOUR AUTH KEY>";
+
+// Toggle to sleep HTTP Server
+const bool enableHttpServer = false;
+
+// Toggle to enable ESP Insights
+// See https://dashboard.insights.espressif.com/
+const bool enableInsights = false;
+
+// Toggle to enable configuration changes via serial interface
+const bool enableSerialConfiguration = true;
+////////////////////////////////////////////////////////////////////////////////
+
+char *deviceIdentifier(int length) {
+    const char *logTag = __func__;
+
+    // Temporary buffer to hold the full UniqueID8 as a hex string
+    // 8 bytes * 2 characters per byte + 1 for null terminator
+    char fullIdentifier[17];
+    int  index = 0;
+
+    for (int i = 0; i < 8; i++) {
+        if (UniqueID8[i] < 0x10) {
+            fullIdentifier[index++] = '0';
+        }
+
+        // Calculate remaining buffer size
+        int remainingBufferSize = sizeof(fullIdentifier) - index;
+
+        // Format the current byte in UniqueID8 as lowercase hexadecimal string
+        snprintf(&fullIdentifier[index], remainingBufferSize, "%02x", UniqueID8[i]);
+        index += 2;
+    }
+
+    // Ensure null termination of the full identifier
+    fullIdentifier[16] = '\0';
+
+    // Calculate start index to get the last `length` characters
+    int startIndex = (16 > length) ? (16 - length) : 0;
+
+    // Dynamically allocate memory for the identifier
+    char *identifier = new char[length + 1];
+
+    // Copy the last `length` characters from the full identifier
+    strncpy(identifier, &fullIdentifier[startIndex], length);
+
+    // Ensure null termination
+    identifier[length] = '\0';
+
+    return identifier;
+}
+
+bool deviceSetup() {
+    const char *logTag = __func__;
+
+    // Store compilation time string
+    snprintf(applicationCompilationString, sizeof(applicationCompilationString), "%s at %s", __DATE__, __TIME__);
+
+    // Print to serial monitor directly as loggers are not yet initialized
+    char initialLogMessage[64];
+    snprintf(initialLogMessage, sizeof(initialLogMessage), "[%s][🏁 START] Initializing device", logTag);
+    Serial.println(initialLogMessage);
+
+    // Explicitly set the log level
+    logSetLevel(static_cast<LogLevel>(CORE_DEBUG_LEVEL));
+
+    // Scan for Wi-Fi networks
+    wifiScanNetworks();
+
+    // Wi-Fi and other initialization code
+    if (!wifiDisconnect()) {
+        logError(logTag, "Failed to disconnect previous Wi-Fi connection");
+
+        return false;
+    }
+
+    if (!wifiConnect()) {
+        logError(logTag, "Failed to establish a Wi-Fi connection");
+
+        return false;
+    }
+
+    if (!displayInit()) {
+        logError(logTag, "Failed to initialize Display");
+
+        return false;
+    }
+
+    if (!wireInit()) {
+        logError(logTag, "Failed to initialize Wire");
+
+        return false;
+    }
+
+    // if (enableInsights) {
+    //    insightsInit(applicationInsightsKey);
+    // }
+
+    // Indicate that setup was successful
+    return true;
+}
+
+void deviceLoop() {
+    const char *logTag = __func__;
+
+    if (enableSerialConfiguration) {
+        logDebug(logTag, "Handling serial input events");
+        //        serialHandleEvents();
+    }
+
+    if (enableHttpServer) {
+        logDebug(logTag, "Handling HTTP Server events");
+        //        httpServerHandleEvents();
+    }
+
+    // Update display
+    const char *lines[4] = {"Line 1", "Line 2", "Line 3", "Line 4"};
+    displayUpdate(lines);
+}
+
+void deviceInformation() {
+    const char *logTag = __func__;
+
+    // TODO(ksatirli) add code here
+}
---
+++ src/Device.h
@@ -0,0 +1,35 @@
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+// Built-in and Third-Party libraries
+#include <Arduino.h>
+#include <ArduinoUniqueID.h>
+#include <esp_system.h>
+
+/**
+ * @brief Retrieves device identifier
+ *
+ * This function retrieves a unique device identifier and formats it for downstream use
+ *
+ * @param length The length of the string to return.
+ *
+ * @return The text representation of the device identifier
+ */
+char *deviceIdentifier(int length = 16);
+
+/**
+ * @brief Initializes the `setup` stage.
+ */
+bool deviceSetup();
+
+/**
+ * @brief Executes the `loop` stage.
+ */
+void deviceLoop();
+
+/**
+ * @brief Gathers device information from ESP32 module.
+*/
+void deviceInformation();
+
+#endif  // DEVICE_H_
