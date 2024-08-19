586633687033c4c7ef7f4ec4a49daa5be7d5cf8b
Kerim Satirli <k.satirli@gmail.com>
2024-08-19 21:24:47 +0100

    linting

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -1,61 +1,47 @@
 #include "HttpServer.h"
 
-#include <WebServer.h>
+// Static member initialization
+WebServer *HttpServer::httpServer = nullptr;
 
-#include "Filesystem.h"
-#include "Log.h"
-
-////////////////////////////////////////////////////////////////////////////////
-// CONFIGURATION                                                              //
-////////////////////////////////////////////////////////////////////////////////
-
-// `7284` is `PATH` on a phone keypad
-const uint16_t httpServerPort = 7284;
-
-// Declare a pointer for the HTTP server for dynamic allocation
-WebServer *httpServer = nullptr;
-////////////////////////////////////////////////////////////////////////////////
-
-bool httpServerInit() {
+bool HttpServer::init() {
     const char *logTag = __func__;
 
     if (!httpServer) {
         httpServer = new WebServer(httpServerPort);
-        logInfo(logTag, "HTTP server initialized on port `%d`", httpServerPort);
+        Log::info(logTag, "HTTP server initialized on port `%d`", httpServerPort);
 
         return true;
     } else {
-        logError(logTag, "HTTP server pointer is null, initialization failed");
+        Log::error(logTag, "HTTP server pointer is null, initialization failed");
 
         return false;
     }
 }
 
-void httpServerMapRoutes() {
+void HttpServer::mapRoutes() {
     const char *logTag = __func__;
 
-    // TODO(ksatirli) dynamically populate these entries from a config
     if (httpServer) {
         // Handle requests to root
-        httpServer->on("/", HTTP_GET, httpServerHandlerOnConnect);
+        httpServer->on("/", HTTP_GET, handlerOnConnect);
 
         // Handle 404 not found errors
-        httpServer->onNotFound(httpServerHandlerNotFound);
+        httpServer->onNotFound(handlerNotFound);
 
         httpServer->begin();
-        logInfo(logTag, "HTTP server routes mapped and server started on port `%d`", httpServerPort);
+        Log::info(logTag, "HTTP server routes mapped and server started on port `%d`", httpServerPort);
     } else {
-        logError(logTag, "HTTP server pointer is null, cannot map routes");
+        Log::error(logTag, "HTTP server pointer is null, cannot map routes");
     }
 }
 
-bool httpServerStreamFile(WebServer *server, const char *filename, const char *contentType) {
+bool HttpServer::streamFile(WebServer *server, const char *filename, const char *contentType) {
     const char *logTag = __func__;
 
-    File file = filesystemStreamFile(filename, "r");
+    File file = Filesystem::streamFile(filename, "r");
 
     if (!file) {
-        logError(logTag, "Failed to open file `%s`", filename);
+        Log::error(logTag, "Failed to open file `%s`", filename);
 
         return false;
     }
@@ -63,31 +49,31 @@ bool httpServerStreamFile(WebServer *server, const char *filename, const char *c
     server->streamFile(file, contentType);
     file.close();
 
-    logInfo(logTag, "File streamed successfully `%s`", filename);
+    Log::info(logTag, "File streamed successfully `%s`", filename);
 
     return true;
 }
 
-void httpServerHandleEvents() {
+void HttpServer::handleEvents() {
     const char *logTag = __func__;
 
     if (httpServer) {
         httpServer->handleClient();
-        logInfo(logTag, "HTTP server handling client interactions");
+        Log::info(logTag, "HTTP server handling client interactions");
     } else {
-        logError(logTag, "HTTP server not initialized");
+        Log::error(logTag, "HTTP server not initialized");
     }
 }
 
-void httpServerHandlerOnConnect() {
+void HttpServer::handlerOnConnect() {
     const char *logTag = __func__;
 
-    if (!httpServerStreamFile(httpServer, "/index.html", "text/html")) {
+    if (!streamFile(httpServer, "/index.html", "text/html")) {
         httpServer->send(500, "text/plain", "Internal Server Error");
     }
 }
 
-void httpServerHandlerNotFound() {
+void HttpServer::handlerNotFound() {
     const char *logTag = __func__;
 
     httpServer->send(404, "text/plain", "404: Not Found");
--- src/HttpServer.h
+++ src/HttpServer.h
@@ -3,58 +3,68 @@
 
 #include <WebServer.h>
 
-/**
- * @brief Global pointer to the HTTP server instance.
- */
-extern WebServer *httpServer;
+#include "Filesystem.h"
+#include "Log.h"
 
 /**
- * @brief Initialize and configure the HTTP server.
- *
- * This function sets up the HTTP server, registers request handlers, and starts the server.
- *
- * @return True if the server was successfully initialized, false otherwise.
+ * @brief HttpServer class for managing HTTP server initialization, route mapping, and request handling.
  */
-bool httpServerInit();
+class HttpServer {
+ private:
+    constexpr static uint16_t httpServerPort = 7284;
 
-/**
- * @brief Map routes for the HTTP server.
- *
- * This function streams a file to the client over HTTP.
- */
-void httpServerMapRoutes();
+    // Pointer to the HTTP server instance
+    static WebServer *httpServer;
 
-/**
- * @brief Stream a file over HTTP.
- *
- * This function streams a file to the client over HTTP.
- *
- * @param server Pointer to the WebServer instance.
- * @param filename Name of the file to stream.
- * @param contentType Content type of the file.
- * @return True if the file was successfully streamed, false otherwise.
- */
-bool httpServerStreamFile(WebServer *server, const char *filename, const char *contentType = "text/html");
+    /**
+     * @brief Handle requests to the root ("/") path.
+     *
+     * This function handles HTTP GET requests to the root path.
+     */
+    static void handlerOnConnect();
 
-/**
- * @brief Begin processing incoming HTTP requests.
- *
- * This function handles incoming HTTP requests and processes them.
- */
-void httpServerHandleEvents();
+    /**
+     * @brief Handle requests to undefined paths.
+     *
+     * This function handles HTTP requests to paths that are not defined.
+     */
+    static void handlerNotFound();
 
-/**
- * @brief Handle requests to the root ("/") path.
- *
- * This function handles HTTP GET requests to the root path.
- */
-void httpServerHandlerOnConnect();
+ public:
+    /**
+     * @brief Initialize and configure the HTTP server.
+     *
+     * This function sets up the HTTP server, registers request handlers, and starts the server.
+     *
+     * @return True if the server was successfully initialized, false otherwise.
+     */
+    static bool init();
 
-/**
- * @brief Handle requests to undefined paths.
- *
- * This function handles HTTP requests to paths that are not defined.
- */
-void httpServerHandlerNotFound();
+    /**
+     * @brief Map routes for the HTTP server.
+     *
+     * This function registers the routes and handlers for the HTTP server.
+     */
+    static void mapRoutes();
+
+    /**
+     * @brief Stream a file over HTTP.
+     *
+     * This function streams a file to the client over HTTP.
+     *
+     * @param server Pointer to the WebServer instance.
+     * @param filename Name of the file to stream.
+     * @param contentType Content type of the file.
+     * @return True if the file was successfully streamed, false otherwise.
+     */
+    static bool streamFile(WebServer *server, const char *filename, const char *contentType = "text/html");
+
+    /**
+     * @brief Begin processing incoming HTTP requests.
+     *
+     * This function handles incoming HTTP requests and processes them.
+     */
+    static void handleEvents();
+};
 
 #endif  // HTTPSERVER_H_
