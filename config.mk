42d7cfd5ea0fd3abaf97ec1c93bdbcc3d4b26621
Kerim Satirli <k.satirli@gmail.com>
2024-09-06 22:00:57 +0200

    updates code quality tooling

--- config.mk
+++ config.mk
@@ -32,14 +32,14 @@ ARDUINO_LIBRARIES_UPSTREAM ?= \
     "Adafruit Unified Sensor@1.1.14" \
     "ArduinoJson@7.1.0" \
     "ArduinoUniqueID@1.3.0" \
-    "ESP32Encoder@0.11.6" \
+    "ESP32Encoder@0.11.7" \
     "ICM20948_WE@1.1.10" \
     "INA219_WE@1.3.8" \
     "LittleFS_esp32@1.0.6" \
     "Ministache@1.0.1" \
     "PID_v2@2.0.1" \
     "SCServo@1.0.2" \
-    "SimpleKalmanFilter@0.1.0"
+    "SimpleKalmanFilter@0.2.0"
 
 # list of unsafe / non-upstream Arduino libraries to be installed
 ARDUINO_LIBRARIES_UNSAFE ?= ""
